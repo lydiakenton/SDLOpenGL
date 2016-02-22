3ca31adbcbc5baf055b414d59f6aff893232d447
lydiakenton <i7217417@bournemouth.ac.uk>
2016-02-22 11:46:09 +0000

    Window opens and crashes

--- SDLOpenGL.pro
+++ SDLOpenGL.pro
@@ -1,10 +1,11 @@
 TARGET=SDLOpenGL
 
 OBJECTS_DIR=obj
-
+INCLUDEPATH += $$PWD/include
 SOURCES+=$$PWD/src/main.cpp \
          $$PWD/src/SDLOpenGL.cpp
 
 HEADERS+=$$PWD/include/SDLOpenGL.h
-
+QMAKE_CXXFLAGS+=$$system(sdl2-config --cflags)
+LIBS+=$$system(sdl2-config --libs)
 include($(HOME)/NGL/UseNGL.pri)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "SDLOpenGL.h"
+#include <ngl/NGLInit.h>
+
+void initializeGL();
+void renderGL();
 
 int main(int argc, char *argv[])
 {
-  std::cout<<"working\n";
+  SDLOpenGL window("test",0,0,1024,720);
+  window.makeCurrent();
+  initializeGL();
+  while(true)
+  {
+    renderGL();
+    window.swapWindow();
+    SDL_Delay(10);
+  }
+}
+
+void initializeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.8,0.8,0.8,1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
